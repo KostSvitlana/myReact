b777edabec443b79a516a6c7060e4b2ff400fc36
Svitlana Kost <1kost.svitlana@gmail.com>
2018-09-06 09:12:05 +0300

    resolved problem with routes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router } from "react-router-dom";
-import { createBrowserHistory } from "history";
+import { BrowserRouter as Router} from "react-router-dom";
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <Router history={createBrowserHistory}> 
+  <Router> 
     <App /> 
   </Router>,
   document.getElementById('root'));
