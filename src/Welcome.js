e25c2c5db9b0f0bec3824fcdd66c0802d2b5038d
Svitlana Kost <s.kost@cleverdec.com>
2018-09-06 15:14:58 +0300

    add actions and reducers

--- src/Welcome.js
+++ src/Welcome.js
@@ -3,23 +3,23 @@ import React, { Component } from 'react';
 import './App.css';
 
 class Welcome extends Component {
-    buttonHendler = () => {
-      alert("Good boy")
-    };
-    render() {
-      return (
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React!!!</h1>
-          </header>
-          <p className="App-intro">
-            Hello, {this.props.name}.
-          </p>
-          <button onClick={this.buttonHendler}> Push me </button>
-        </div>
-      )
-    }
+  buttonHendler = () => {
+    alert("Good boy")
+  };
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React!!!</h1>
+        </header>
+        <p className="App-intro">
+          Hello, {this.props.name}.
+        </p>
+        <button onClick={this.buttonHendler}> Push me </button>
+      </div>
+    )
   }
+}
 
-  export default Welcome;
+export default Welcome;
